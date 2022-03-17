25d770c0777ae5527b02e32fe984422ffb70df1d
PHILOMINA <philomina.jeni@gmail.com>
2022-03-17 16:20:36 +0530

    v1

--- mongooseSchema.js
+++ mongooseSchema.js
@@ -1,38 +1,38 @@
 const mongoose = require('mongoose');
-// const validator = require('validator');
+
 const opts = {
     timestamps: {
-      createdAt: 'created_at',
-      updatedAt: 'updated_at'
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
     }
-  };
+};
 
 var userSchema = new mongoose.Schema(
     {
-firstName:{
-    type:String,
-    required:true
-},
-lastName:{
-    type:String
-},
-email:{
-    type:String,
-    required:true,
-   unique:true
-},
-dob:{
-    type:Date
-},
-password:{
-    type:String,
-    required:true
-},
-isDeleted:{
-    type:Boolean,
-    default:false
-}
- },opts)
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        dob: {
+            type: Date
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false
+        }
+    }, opts)
 
- const userDetails = mongoose.model('userscoll',userSchema); //collection name here
- module.exports={userDetails}
+const userDetails = mongoose.model('userscoll', userSchema); //collection name here
+module.exports = { userDetails }
